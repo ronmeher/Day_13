c299a91b312baf8472aa1845e402aedb3bebf71c
ronmeher <ronmeher@gmail.com>
2022-06-01 19:39:08 +0530

    Day 13

---
+++ Generics/GenericMax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Generics
+{
+    public class GenericMax<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMax(T[] value)
+        {
+            this.value = value;
+
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValues(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValues(this.value);
+            return max;
+        }
+        public void printMaxValue()
+        {
+            var max = MaxValues(this.value);
+            Console.WriteLine("Maximum Value is:  " + max);
+        }
+
+    }
+}
---
+++ Generics/MainClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 112, 223, 334, 445, 556 };
+            GenericMax<int> gen = new GenericMax<int>(arr);
+            gen.printMaxValue();
+            Console.ReadLine();
+
+        }
+    }
+}
